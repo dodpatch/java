2e1ad24d458086ae1c0ac631faa12ca68ae5661a
Digital <paulido92@gmail.com>
2019-06-05 15:40:06 +0000

    "EtudiantDAO debug"

---
+++ src/DAO/EtudiantDAO.java
@@ -0,0 +1,207 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author DIGITAL
+ */
+import DAO.DAO;
+import java.util.ArrayList;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import models.Etudiant;
+import models.Filiere;
+//CTRL + SHIFT + O pour générer les imports
+public class EtudiantDAO implements DAO<Etudiant> {
+  private final DaoFactory daoFactory;
+  EtudiantDAO(DaoFactory daoFactory)
+  {
+      this.daoFactory=daoFactory;
+  }
+  @Override
+  public boolean create(Etudiant etd) {
+      try
+      { 
+          Connection connexion=daoFactory.getConnection();
+          Statement stmt=connexion.createStatement();
+          String sql="insert into etudiants(ID_FIL,NOM,PRENOM,DATE,LIEU,SEXE,MATRICLUE) values("+
+           etd.getFiliere().getId()+",'"+
+           etd.getNom()+"','"+
+           etd.getPrenom()+"','"+
+           etd.getDate()+"','"+
+           etd.getLieu()+"','"+
+           etd.getSexe()+"','"+
+           etd.getMatricule()+"','"+
+           etd.getNiveau() +"')" ; 
+           stmt.executeUpdate(sql);
+           System.out.println("operation effectuée avec succes");
+           return true;
+      }
+      catch(SQLException e)
+      {
+          e.printStackTrace();
+      }
+    return false;
+  }
+
+    /**
+     *
+     * @param field
+     * @param value
+
+     * @return
+     */
+    @Override
+  public boolean delete(int id) 
+  {
+      Connection connexion=null;
+      PreparedStatement stmt=null;
+       try 
+       {
+            connexion=daoFactory.getConnection();
+            stmt=connexion.prepareStatement("delete FROM etudiants where id = "+ id +"");
+            stmt.executeUpdate();
+            return true;
+        }
+       catch(SQLException e)
+       {
+           e.printStackTrace();
+       }
+       return false;
+  }
+   
+  public boolean update(Etudiant etd, int id) {
+      Connection connexion=null;
+      Statement stmt=null;
+       try 
+       {
+          connexion=daoFactory.getConnection();
+          stmt=connexion.createStatement();
+          String sql="update etudiants set "+ 
+                  "id_fil ="+ etd.getFiliere().getId()+ ","+
+                  "nom='"+ etd.getNom()+"',"+ 
+                  "prenom='"+ etd.getPrenom()+"',"+ 
+                  "date='"+ etd.getDate()+"',"+ 
+                  "lieu='"+ etd.getLieu()+"',"+ 
+                  "sexe='"+ etd.getSexe()+"',"+ 
+                  "matricule='"+ etd.getMatricule()+"',"+ 
+                  "niveau='"+ etd.getNiveau()+"',"+ 
+                  " where id = " + id +"";
+          stmt.executeUpdate(sql);
+          return true;
+        }
+       catch(SQLException e)
+       {
+           e.printStackTrace();
+       }
+    return false;
+  }
+   
+  @Override
+   public Etudiant find(String field, String value) 
+  {
+        Etudiant etudiant=null;  
+        String sql=null;
+        //Connection connexion=null;
+       // PreparedStatement stmt=null;
+        ResultSet rs=null;
+
+        try 
+        {
+            
+            Connection connexion=daoFactory.getConnection();
+            Statement stmt=connexion.createStatement();
+//            "+var+"="+value;
+            sql="select * from etudiants where "+field+"='"+value+"'"; 
+            
+            rs=stmt.executeQuery(sql);
+            if(rs.first())
+            {
+                sql = "select * from filieres where ID_FIL = " + rs.getInt("ID_FIL") +"";
+                ResultSet r = stmt.executeQuery(sql);
+                Filiere fil = new Filiere(r.getInt("ID_FIL"), rs.getString("FILIERE"));
+                etudiant = new Etudiant(
+                rs.getInt("ID_ETD"),
+                rs.getString("nom"),
+                rs.getString("prenom"),
+                rs.getString("date"),
+                rs.getString("lieu"),
+                rs.getString("sexe"),
+                rs.getString("matricule"),
+                fil,
+                rs.getString("niveau")); 
+                // recuperation de la note
+                sql = "select NOTE, ID_MAT from notes where ID_ETD = " + rs.getInt("ID_ETD") + ""; 
+                rs = stmt.executeQuery(sql);
+                sql = "select INTITULE form matieres where ID_MAT = " + rs.getInt("ID_MAT") + "";
+                r = stmt.executeQuery(sql);
+                HashMap list = new HashMap();
+                if(rs.first() && r.first())
+                    list.put(r.getString("INTITULE"), rs.getFloat("NOTE"));
+                etudiant.setListNote(list);
+            }
+        }
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+        }
+        return etudiant;
+   }
+  public ArrayList<Etudiant> getAll(){
+      Etudiant etudiant=null;
+      ArrayList<Etudiant> listEtudiant=null;
+      Connection connexion=null;
+      PreparedStatement stmt=null;
+      ResultSet rs=null, r = null, rsNote = null, rsMat = null;
+      String sql;
+    try 
+    {
+        connexion = daoFactory.getConnection();
+        stmt = connexion.prepareStatement("Select * from etudiants");
+        rs = stmt.executeQuery();
+        listEtudiant = new ArrayList<Etudiant>();
+        while(rs.next())
+        {
+//            String getN="select NIVEAU from niveaux where ID_NV = "+ rs.getInt("ID_ETD") +"";
+//            String getFil = "select filiere from filieres where ID_FIL = " + rs.getInt("ID_FIL") +"";
+//            String getDep = "select DEP from niveaux where ID_DEP = " + rs.getInt("ID_DEP") +"";
+//            stmt = connexion.prepareStatement(getN);
+//            ResultSet rsgetN = stmt.executeQuery();
+//            ResultSet rsgetFil = stmt.executeQuery();
+//            ResultSet rsgetDep = stmt.executeQuery();
+            sql = "select * from filieres where ID_FIL = " + rs.getInt("ID_FIL") + "";
+            r = stmt.executeQuery(sql);
+            Filiere fil = new Filiere(r.getInt("ID_FIL"), r.getString("FILIERE"));
+            etudiant = new Etudiant(
+            rs.getInt("ID_ETD"),
+            rs.getString("NOM"),
+            rs.getString("PRENOM"),
+            rs.getString("DATE"),
+            rs.getString("LIEU"),
+            rs.getString("SEXE"),
+            rs.getString("MATRICULE"),
+            fil,
+            rs.getString("NIVEAU"));
+            sql = "select NOTE, ID_MAT from notes where ID_ETD = " + rs.getInt("ID_ETD") + ""; 
+            rsNote = stmt.executeQuery(sql);
+            sql = "select INTITULE form matieres where ID_MAT = " + rs.getInt("ID_MAT") + "";
+            rsMat = stmt.executeQuery(sql);
+            HashMap list = new HashMap();
+            if(rsNote.first() && rsMat.first())
+                list.put(rsMat.getString("INTITULE"), rsNote.getFloat("NOTE"));
+            etudiant.setListNote(list);
+            listEtudiant.add(etudiant);
+        }
+    }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    return listEtudiant;
+  }
+}
